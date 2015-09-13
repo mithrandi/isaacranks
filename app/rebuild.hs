192e4500da7c31f2a219efb53e8e6dc04bfd81b2
Tristan Seligmann <mithrandi@mithrandi.net>
2015-09-13 17:30:01 +0200

    Make rebuildranks runnable under stack.

--- app/rebuild.hs
+++ app/rebuild.hs
@@ -4,6 +4,8 @@ import           Application (makeFoundation)
 import           Control.Monad.Logger (runLoggingT)
 import           Control.Monad.Logger (runStdoutLoggingT)
 import qualified Database.Persist
+import           Database.Persist.Postgresql (createPostgresqlPool, pgConnStr, pgPoolSize)
+import           Helpers.Heroku (herokuConf)
 import           Import
 import           Vote (reprocessVotes)
 import           Yesod.Default.Config
@@ -11,5 +13,11 @@ import           Yesod.Default.Config
 main :: IO ()
 main = do
   conf <- Yesod.Default.Config.loadConfig (configSettings Production) { csParseExtra = parseExtra }
-  foundation <- makeFoundation conf
-  runStdoutLoggingT $ Database.Persist.runPool (persistConfig foundation) reprocessVotes (connPool foundation)
+  dbconf <- if development
+           then withYamlEnvironment "config/postgresql.yml" (appEnv conf)
+                Database.Persist.loadConfig >>=
+                Database.Persist.applyEnv
+           else herokuConf
+  pool <- runStdoutLoggingT
+         $ createPostgresqlPool (pgConnStr dbconf) (pgPoolSize dbconf)
+  runStdoutLoggingT $ Database.Persist.runPool dbconf reprocessVotes pool
