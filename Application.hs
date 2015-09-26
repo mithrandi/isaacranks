d6b10e084e82d9b1a56dd894cba3af95fdb7b07d
Tristan Seligmann <mithrandi@mithrandi.net>
2015-09-26 03:38:27 +0200

    Compress static files too.

--- Application.hs
+++ Application.hs
@@ -15,18 +15,23 @@ import           Database.Persist.Sql (runMigration)
 import           Helpers.Heroku (herokuConf)
 import           Import
 import           Network.HTTP.Client.Conduit (newManager)
+import           Network.Wai (Middleware)
 import           Network.Wai.Logger (clockDateCacher)
+import           Network.Wai.Middleware.AcceptOverride (acceptOverride)
+import           Network.Wai.Middleware.Autohead (autohead)
+import           Network.Wai.Middleware.Gzip (gzip, gzipFiles, GzipFiles(GzipCompress))
+import           Network.Wai.Middleware.MethodOverride (methodOverride)
 import           Network.Wai.Middleware.RequestLogger
     ( mkRequestLogger, outputFormat, OutputFormat (..), IPAddrSource (..), destination
     )
 import qualified Network.Wai.Middleware.RequestLogger as RequestLogger
 import           System.Environment (getEnv)
 import           System.Log.FastLogger (newStdoutLoggerSet, defaultBufSize)
+import qualified Web.ClientSession as WS
 import           Yesod.Core.Types (loggerSet, Logger (Logger))
 import           Yesod.Default.Config
 import           Yesod.Default.Handlers
 import           Yesod.Default.Main
-import qualified Web.ClientSession as WS
 
 -- Import all relevant handler modules here.
 -- Don't forget to add new modules to your cabal file!
@@ -38,6 +43,12 @@ import           Handler.Donate
 -- of the call to mkYesodData which occurs in Foundation.hs. Please see the
 -- comments there for more details.
 mkYesodDispatch "App" resourcesApp
+
+myMiddlewares :: Middleware
+myMiddlewares = acceptOverride
+                . autohead
+                . gzip def {gzipFiles = GzipCompress}
+                . methodOverride
 
 -- This function allocates resources (such as a database connection pool),
 -- performs initialization and creates a WAI application. This is also the
@@ -59,7 +70,7 @@ makeApplication conf = do
     -- Create the WAI application and apply middlewares
     app <- toWaiAppPlain foundation
     let logFunc = messageLoggerSource foundation (appLogger foundation)
-    return (logWare $ defaultMiddlewaresNoLogging app, logFunc)
+    return (logWare $ myMiddlewares app, logFunc)
 
 
 -- | Loads up any necessary settings, creates your foundation datatype, and
