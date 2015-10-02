82a0d2f53588cf55560cc85531188e8d7f203c76
Tristan Seligmann <mithrandi@mithrandi.net>
2015-10-02 16:47:12 +0200

    Implement optimize replay protection.

--- Handler/Vote.hs
+++ Handler/Vote.hs
@@ -1,36 +1,27 @@
 module Handler.Vote where
 
 import           Control.Lens hiding ((.=))
-import           Data.Aeson (encode)
-import           Data.Binary.Get (runGet, getWord32be)
-import           Data.Binary.Put (runPut, putWord32be)
-import qualified Data.ByteString as B
 import qualified Data.ByteString.Char8 as BC
-import           Data.ByteString.Lazy (toStrict, fromStrict)
 import           Data.List (last)
 import           Data.Maybe
 import qualified Data.Text as T
-import           Data.Text.Encoding (decodeUtf8)
 import           Data.Time (getCurrentTime)
 import           Import
 import           Model.IsaacVersion
 import           Network.Wai (requestHeaders, remoteHost)
 import           System.Random (newStdGen)
 import           System.Random.Shuffle (shuffle')
-import           Vote (processVote)
-import qualified Web.ClientSession as WS
-
-jsonToText :: ToJSON a => a -> Text
-jsonToText = decodeUtf8 . toStrict . encode
+import           Vote (processVote, encryptBallot, decryptBallot)
 
 getVoteR :: IsaacVersion -> Handler TypedContent
 getVoteR ver = do
   items <- runDB $ selectList [ItemVersion ==. ver] []
   gen <- lift newStdGen
-  let (Entity _ left):(Entity _ right):_ = shuffle' items (length items) gen
+  let Entity _ left:Entity _ right:_ = shuffle' items (length items) gen
   alreadyExpired
-  ballotLeft <- encryptBallot (left^.itemIsaacId) (right^.itemIsaacId)
-  ballotRight <- encryptBallot (right^.itemIsaacId) (left^.itemIsaacId)
+  timestamp <- lift getCurrentTime
+  ballotLeft <- encryptBallot timestamp (left^.itemIsaacId) (right^.itemIsaacId)
+  ballotRight <- encryptBallot timestamp (right^.itemIsaacId) (left^.itemIsaacId)
   let ballotJson = object
                    [ "left" .= left
                    , "ballotLeft" .= ballotLeft
@@ -44,30 +35,6 @@ getVoteR ver = do
       $(widgetFile "vote")
     provideJson ballotJson
 
-encodeBallot :: Int -> Int -> B.ByteString
-encodeBallot winner loser = toStrict . runPut $ do
-  putWord32be (fromIntegral winner)
-  putWord32be (fromIntegral loser)
-
-decodeBallot :: B.ByteString -> (Int, Int)
-decodeBallot = go . fromStrict
-  where go = runGet $ do
-          winner <- getWord32be
-          loser <- getWord32be
-          return (fromIntegral winner, fromIntegral loser)
-
-encryptBallot :: Int -> Int -> Handler Text
-encryptBallot winner loser = do
-  k <- ballotKey <$> getYesod
-  out <- liftIO $ WS.encryptIO k (encodeBallot winner loser)
-  return . T.pack . BC.unpack $ out
-
-decryptBallot :: Text -> Handler (Int, Int)
-decryptBallot b = do
-  k <- ballotKey <$> getYesod
-  let Just b' = WS.decrypt k (BC.pack . T.unpack $ b)
-  return $ decodeBallot b'
-
 postVoteR :: IsaacVersion -> Handler TypedContent
 postVoteR ver = do
   request <- waiRequest
@@ -76,10 +43,8 @@ postVoteR ver = do
               (T.pack . show $ remoteHost request)
               (T.stripStart . T.stripEnd . last . T.splitOn ",")
               value
-  (ballot, fancy) <- runInputPost $
-                    (,) <$> ireq textField "ballot"
-                        <*> ireq checkBoxField "fancy"
-  (winner, loser) <- decryptBallot ballot
+  ballot <- runInputPost $ ireq textField "ballot"
   timestamp <- lift getCurrentTime
-  _ <- runDB (processVote ver winner loser timestamp voter ballot fancy)
+  (winner, loser) <- decryptBallot timestamp ballot
+  runDB (processVote ver winner loser timestamp voter ballot)
   getVoteR ver
--- Vote.hs
+++ Vote.hs
@@ -6,20 +6,26 @@ import           Control.Monad (forM_)
 import           Control.Monad.Trans.Reader (ReaderT)
 import           Data.Aeson (encode)
 import           Data.Aeson.Lens (_JSON, _Object, _Array)
+import           Data.Binary.Get (runGet, getWord32be)
+import           Data.Binary.Put (runPut, putWord32be)
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Char8 as BC
+import           Data.ByteString.Lazy (toStrict, fromStrict)
 import qualified Data.ByteString.Lazy as LBS
 import           Data.List (foldl')
 import qualified Data.Map.Strict as M
 import           Data.Maybe (fromMaybe)
 import qualified Data.Text as T
 import qualified Data.Text.IO as TI
-import           Data.Time (UTCTime)
-import           Data.Time.Clock.POSIX (getPOSIXTime)
+import           Data.Time (getCurrentTime, UTCTime, NominalDiffTime, addUTCTime)
+import           Data.Time.Clock.POSIX (getPOSIXTime, posixSecondsToUTCTime, utcTimeToPOSIXSeconds)
 import           Database.Persist.Sql (SqlBackend)
 import           Import
 import           Model.IsaacVersion
 import           Network.AWS
 import           Network.AWS.S3
 import           System.Environment (lookupEnv)
+import qualified Web.ClientSession as WS
 
 k :: Double
 k = 24.0
@@ -30,11 +36,13 @@ adjustment s e = k * (s - e)
 expected :: Double -> Double -> Double
 expected r1 r2 = 1 / (1 + 10 ** ((r2 - r1) / 400))
 
-processVote :: MonadIO m => IsaacVersion -> Int -> Int -> UTCTime -> Text -> Text -> Bool -> ReaderT SqlBackend m (Key Vote)
-processVote ver w l timestamp voter rawBallot fancy = do
+processVote :: MonadIO m => IsaacVersion -> Int -> Int -> UTCTime -> Text -> Text -> ReaderT SqlBackend m ()
+processVote ver w l timestamp voter rawBallot = do
   Just (Entity w' _) <- getBy $ UniqueItem ver w
   Just (Entity l' _) <- getBy $ UniqueItem ver l
-  insert $ Vote ver w' l' timestamp voter (Just rawBallot) (Just fancy)
+  _ <- insert $ Ballot rawBallot timestamp
+  _ <- insert $ Vote ver w' l' timestamp voter
+  return ()
 
 applyVote :: M.Map (Key Item) Item -> Vote -> M.Map (Key Item) Item
 applyVote items vote =
@@ -57,6 +65,8 @@ reprocessVotes = do
   votes <- map entityVal <$> selectList [] [Asc VoteTimestamp]
   forM_ (M.toList $ foldl' applyVote items votes) $
     \(itemId, Item {_itemRating = r, _itemVotes = v}) -> update itemId [ItemRating =. r, ItemVotes =. v]
+  now <- liftIO getCurrentTime
+  deleteWhere [BallotTimestamp <=. (-validTime) `addUTCTime` now]
   return (items, votes)
 
 serializeVotes :: M.Map (Key Item) Item -> [Vote] -> Value
@@ -106,3 +116,38 @@ storeDump :: Text -> Text -> UTCTime -> (MonadIO m) => ReaderT SqlBackend m ()
 storeDump bucketName name timestamp = do
   _ <- insert $ Dump bucketName name timestamp
   return ()
+
+encodeBallot :: UTCTime -> Int -> Int -> B.ByteString
+encodeBallot expiry winner loser = toStrict . runPut $ do
+  putWord32be (truncate . utcTimeToPOSIXSeconds $ expiry)
+  putWord32be (fromIntegral winner)
+  putWord32be (fromIntegral loser)
+
+decodeBallot :: B.ByteString -> (UTCTime, Int, Int)
+decodeBallot = go . fromStrict
+  where go = runGet $ do
+          expiry <- getWord32be
+          winner <- getWord32be
+          loser <- getWord32be
+          return ( posixSecondsToUTCTime (fromIntegral expiry)
+                 , fromIntegral winner
+                 , fromIntegral loser)
+
+-- 1 hour
+validTime :: NominalDiffTime
+validTime = 3600
+
+encryptBallot :: UTCTime -> Int -> Int -> Handler Text
+encryptBallot now winner loser = do
+  key <- ballotKey <$> getYesod
+  out <- liftIO $ WS.encryptIO key (encodeBallot (validTime `addUTCTime` now) winner loser)
+  return . T.pack . BC.unpack $ out
+
+decryptBallot :: UTCTime -> Text -> Handler (Int, Int)
+decryptBallot now b = do
+  key <- ballotKey <$> getYesod
+  let Just b' = WS.decrypt key (BC.pack . T.unpack $ b)
+      (expiry, winner, loser) = decodeBallot b'
+  if expiry < now
+    then error "expired ballot!"
+    else return (winner, loser)
