1298b01aa70ef58fb36d0f4cdba9ef9396d95001
Tristan Seligmann <mithrandi@mithrandi.net>
2017-02-22 21:52:50 +0200

    Update Webpack and stuff.

--- js/webpack.config.js
+++ js/webpack.config.js
@@ -2,24 +2,26 @@ var path = require('path');
 var webpack = require('webpack');
 
 
-module.exports =
-{ entry: path.resolve(__dirname)
-, output:
-  { path: path.resolve(__dirname, '..', 'static', 'js')
-  , filename: 'bundle.js'
-  }
-, devtool: '#cheap-module-eval-source-map'
-, plugins:
-  [ new webpack.ProvidePlugin(
-      { 'fetch': 'imports?this=>global!exports?global.fetch!whatwg-fetch' })
-  ]
-, module:
-  { loaders:
-    [ { test: /\.js$/
-      , loader: 'babel-loader'
-      , exclude: /node_modules/
-      , include: __dirname
+module.exports = function(env) {
+    return (
+    { entry: path.resolve(__dirname)
+    , output:
+      { path: path.resolve(__dirname, '..', 'static', 'js')
+      , filename: 'bundle.js'
       }
-    ]
-  }
-};
+    , devtool: env == 'production' ? 'source-map' : 'cheap-module-eval-source-map'
+    , plugins:
+      [ new webpack.ProvidePlugin(
+          { 'fetch': 'imports-loader?this=>global!exports-loader?global.fetch!whatwg-fetch' })
+      ]
+    , module:
+      { rules:
+        [ { test: /\.js$/
+          , loader: 'babel-loader'
+          , exclude: /node_modules/
+          , include: __dirname
+        }
+        ]
+      }
+    });
+}
