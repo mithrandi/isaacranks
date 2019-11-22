cf603778117d1fcd154a13a4586021d2cfe4552f
Tristan Seligmann <mithrandi@mithrandi.net>
2019-11-22 23:15:58 +0200

    Speed up ranks dramatically.

--- Handler/Ranks.hs
+++ Handler/Ranks.hs
@@ -1,18 +1,17 @@
 module Handler.Ranks (getRanksR) where
 
+import Control.Lens (sumOf, folded)
 import Data.List (genericLength)
 import Import
 import Model.IsaacVersion
 
 getRanksR :: IsaacVersion -> Handler TypedContent
 getRanksR ver = do
-  let ranks :: [Integer]
-      ranks = [1..]
-  (items, votesCast) <- runDB $ (,)
-    <$> ((ranks `zip`) <$> selectList [ItemVersion ==. ver] [Desc ItemRating])
-    <*> count [VoteVersion ==. ver]
-  let items' = map (entityVal . snd) items
-      totalItems = genericLength items
+  items <- runDB $
+    selectList [ItemVersion ==. ver] [Desc ItemRating]
+  let items' = map entityVal items
+      totalItems = genericLength items'
+      votesCast = sumOf (folded . itemVotes) items' `div` 2
       meanVotes :: Double
       meanVotes = fromIntegral (votesCast * 2) / totalItems
   selectRep $ do
